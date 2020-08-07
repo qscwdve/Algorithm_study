7ace59529e711ae525a7ae25b13490598992b00e
jelcream <43948280+Jelcream@users.noreply.github.com>
2020-08-08 01:26:45 +0900

    수 찾기
    백준 1920번 자바 풀이, StringBuilder 사용

---
+++ jelcreams/BOJ_1920/Main.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+	static int[]  arr;
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		arr = new int[N];
+		for(int i = 0; i< N ; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		
+		int M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		while(st.hasMoreTokens()) {
+			if(binary_Search(Integer.parseInt(st.nextToken())))sb.append("1\n");
+			else sb.append("0\n");
+		}
+		System.out.println(sb);
+		
+	}
+	static boolean binary_Search(int find) {
+		int pivot = (arr.length)/2+1;
+		int left = 0, right = arr.length-1;
+		if(find < arr[left] || arr[right]<find) return false;
+		while(left<=right) {
+			if(arr[pivot] == find) {
+				return true;
+			}
+			else if(arr[pivot] > find) {
+				right = pivot-1;
+			}else {
+				left = pivot+1;
+			}
+			pivot = (right+left)/2;
+		}
+		return false;
+	}
+}
