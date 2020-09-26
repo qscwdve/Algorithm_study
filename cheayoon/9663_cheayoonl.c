c0a4c168b2ad9f32927d705fc7e4d11fe96bbc9d
chaeyoonl <48426518+chaeyoonl@users.noreply.github.com>
2020-09-26 13:47:54 +0900

    9663_N-Queen
    N-Queen 문제는 크기가 N × N인 체스판 위에 퀸 N개를 서로 공격할 수 없게 놓는 문제이다.
    
    N이 주어졌을 때, 퀸을 놓는 방법의 수를 구하는 프로그램을 작성하시오.

---
+++ cheayoon/9663_cheayoonl.c
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <algorithm>
+#include <stdio.h>
+#include <queue>
+#include <algorithm>
+#include <tuple>
+#include <time.h>
+#include <math.h>
+#pragma warning (disable:4996)
+
+using namespace std;
+
+
+int col[15];
+int N, total = 0;
+
+bool check(int level) {
+	for (int i = 0; i < level; i++) {
+		if (col[i] == col[level] || abs(col[level] - col[i]) == level - i)// 대각선이거나 같은 라인
+			return false;
+    }
+	//col[i]가 의미하는 것이 X좌표, i가 의미하는것이 Y좌표이므로 차이가 일정하다면 대각선에 있다고 볼 수 있다.
+	return true;
+}
+
+void nqueen(int x) {
+	if (x == N)
+		total++;
+	else {
+		for (int i = 0; i < N; i++) {
+			col[x] = i; // 해당 위치에 퀸을 배치
+			if (check(x)) // 유효하다면 다음행의 퀸 배치, 유효하지않다면 다른 위치로 퀸 배치 변경
+				nqueen(x + 1);
+		}
+	}
+}
+
+int main() {
+	scanf("%d", &N);
+	nqueen(0);
+
+	printf("%d", total);
+
+
+
+	getchar();
+	getchar();
+	return 0;
+}
+​
