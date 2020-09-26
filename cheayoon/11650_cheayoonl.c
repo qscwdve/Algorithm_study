dacc60535ab96cfbf3438090bd5baa6d68732f03
chaeyoonl <48426518+chaeyoonl@users.noreply.github.com>
2020-09-26 13:24:10 +0900

    11650_단어정렬하기
    2차원 평면 위의 점 N개가 주어진다. 좌표를 x좌표가 증가하는 순으로, x좌표가 같으면 y좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.

---
+++ cheayoon/11650_cheayoonl.c
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <algorithm>
+#include <stdio.h>
+#include <queue>
+#include <algorithm>
+#pragma warning (disable:4996)
+
+using namespace std;
+
+
+int num, h, u;
+
+
+int main() {
+	vector<pair<int, int>> p;
+
+
+
+	scanf("%d", &num);
+	for (int i = 0; i < num; i++) {
+		scanf("%d %d", &h, &u);
+		p.push_back(make_pair(h, u));
+
+	}
+	sort(p.begin(), p.end()); 
+
+	for (int i = 0; i < num; i++) {
+		printf("%d %d\n", p[i].first, p[i].second);
+
+	}
+
+	getchar();
+	getchar();
+	return 0;
+}
