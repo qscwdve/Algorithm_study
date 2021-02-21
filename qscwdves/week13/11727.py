2dd55f5fcfe6a100ad7c8c983ecf672348aee590
qscwdve <zlzldbsgml@naver.com>
2021-02-22 03:18:22 +0900

    2*n 타일링2

---
+++ qscwdves/week13/11727.py
@@ -0,0 +1,24 @@
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
+
+n = int(input())
+F = [1, 1]
+ans = 1
+for i in range(2, n):
+    F.append((F[i - 1]*i)%10007)
+for k in range(1, n//2 + 1):
+    one = n - 2*k
+    num = (F[one] * F[k])%10007
+    ans += (2**k * F[one + k] * modinv(num, 10007)) % 10007
+print(ans % 10007)
